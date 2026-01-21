39e573875dabd2a115122e01e1375f9a6764e92b
LucaRoescheisen <luca.roescheisen@gmail.com>
2026-01-21 12:42:43 +1100

    Fixed SPI

--- src/spi_master.v
+++ src/spi_master.v
@@ -1,4 +1,5 @@
 module spi_master (
+    input clk,
     input CS,           //IO10
     input MOSI,         //IO11
     input SCK,           //IO13
@@ -14,16 +15,29 @@ module spi_master (
     reg[2:0] addr_ptr = 0;
         //We want to process data on posedge of received data
 
+    reg sck_sync0, sck_sync1;
+    reg mosi_sync0, mosi_sync1;
+    reg cs_sync0, cs_sync1;
+    always @(posedge clk) begin
+        sck_sync0 <= SCK;
+        sck_sync1 <= sck_sync0;
+        
+        mosi_sync0 <= MOSI;
+        mosi_sync1 <= mosi_sync0;
+        
+        cs_sync0   <= CS;
+        cs_sync1   <= cs_sync0;
+    end
+    wire sck_rise = sck_sync0 & ~sck_sync1;
 
-always @(posedge SCK or posedge CS) begin 
-    if(CS == 1) begin 
+always @(posedge clk) begin 
+    if(cs_sync1 == 1) begin 
         addr_ptr <= 7;
         temp_data <= 0;
         received_data <= 0;
     end
-    else begin
-        temp_data[addr_ptr] <= MOSI;    //LSB to MSB
-
+    else if (sck_rise) begin
+        temp_data[addr_ptr] <= mosi_sync1;    //LSB to MSB
         if(addr_ptr ==  0) begin
             received_data <= 1'b1;
             addr_ptr <= 7;
--- tcl/build.tcl
+++ tcl/build.tcl
@@ -7,12 +7,13 @@ read_verilog "src/ray_calc.v"
 read_verilog "src/ray_feeder.v"
 read_verilog "src/spi_master.v"
 read_verilog "src/world.v"
+read_verilog "src/update_player_pos.v"
 
 # Read constraints
 read_xdc "D:/HDL_Environment/constraints/arty.xdc"
 
 # Synthesize
-synth_design -top vga_top -part xc7s50csga324-1
+synth_design -top vga_top -part xc7s50csga324-1 
 
 
 # Implement
